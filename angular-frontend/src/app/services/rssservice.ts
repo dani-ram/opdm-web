6aa954d65f33e8b3e8fb7a8086366e70b2198dd4
dani-ram <daniramirez1994@gmail.com>
2026-04-21 17:29:32 +0200

    fixing service

--- angular-frontend/src/app/services/rssservice.ts
+++ angular-frontend/src/app/services/rssservice.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class Rssservice {
   
-  private apiUrl = 'https://opdm-web-1.onrender.com/episodios';
+  private apiUrl = 'https://opdm-web-1.onrender.com/programas';
   constructor(private http: HttpClient){}
 
   getepisodios(): Observable<any[]> { 
